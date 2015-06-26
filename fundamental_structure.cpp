87f902fa9307022f7660c3e08978227fd755743d
bothbossandboss <bothboss21@gmail.com>
2015-06-26 18:18:28 +0900

    教科書にある参照のコードを実装(意味なし)

--- fundamental_structure.cpp
+++ fundamental_structure.cpp
@@ -5,18 +5,33 @@
 #include <assert.h>
 #include <iostream>
 
+#define MAX_STUDENT_COUNT 100
 using namespace std;
-
-#define MAX_NAME_SIZE 128
 
 struct student_data{
 	int id;
-	char name[MAX_NAME_SIZE];
+	char *name;
 	int score;
 };
 
-typedef student_data StudentData;
+typedef struct student_data StudentData;
+
+StudentData studentArray[MAX_STUDENT_COUNT];
+
+StudentData refer_data(int i){
+	assert(i >= 0 && i < MAX_STUDENT_COUNT);
+	return studentArray[i];
+}
 
 int main(int argc, char *argv[]){
+	srand((unsigned)time(NULL));
+	for(int i=0;i<MAX_STUDENT_COUNT;i++){
+		char c = 65 + i;
+		studentArray[i].id = i;
+		studentArray[i].name = &c;
+		studentArray[i].score = rand() % 100;
+	}
+	StudentData s = refer_data(rand() % MAX_STUDENT_COUNT);
+	printf("%d, %s => %d\n", s.id, s.name, s.score);
 	return 0;
 }
