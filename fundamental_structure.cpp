5cc2f47243b2d708a6d6f1bd8cd5af081c456681
bothbossandboss <bothboss21@gmail.com>
2015-06-26 18:28:45 +0900

    現在の生徒数を格納する変数を用意

--- fundamental_structure.cpp
+++ fundamental_structure.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 
 #define MAX_STUDENT_COUNT 100
+
 using namespace std;
 
 struct student_data{
@@ -17,21 +18,24 @@ struct student_data{
 typedef struct student_data StudentData;
 
 StudentData studentArray[MAX_STUDENT_COUNT];
+int nowStudentNum = 0;
 
 StudentData refer_data(int i){
-	assert(i >= 0 && i < MAX_STUDENT_COUNT);
+	assert(i >= 0 && i < nowStudentNum);
 	return studentArray[i];
 }
 
 int main(int argc, char *argv[]){
 	srand((unsigned)time(NULL));
-	for(int i=0;i<MAX_STUDENT_COUNT;i++){
+	nowStudentNum = rand() % MAX_STUDENT_COUNT;
+	printf("first : %d\n", nowStudentNum);
+	for(int i=0;i<nowStudentNum;i++){
 		char c = 65 + i;
 		studentArray[i].id = i;
 		studentArray[i].name = &c;
 		studentArray[i].score = rand() % 100;
 	}
-	StudentData s = refer_data(rand() % MAX_STUDENT_COUNT);
+	StudentData s = refer_data(rand() % nowStudentNum);
 	printf("%d, %s => %d\n", s.id, s.name, s.score);
 	return 0;
 }
