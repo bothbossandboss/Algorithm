02338bc2f7e6ea046046d27c4e99ce27e2a70231
bothbossandboss <bothboss21@gmail.com>
2015-06-26 18:01:20 +0900

    2章基本データ構造用のコードとgitignoreを追加

---
+++ fundamental_structure.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <assert.h>
+#include <iostream>
+
+using namespace std;
+
+#define MAX_NAME_SIZE 128
+
+struct student_data{
+	int id;
+	char name[MAX_NAME_SIZE];
+	int score;
+};
+
+typedef student_data StudentData;
+
+int main(int argc, char *argv[]){
+	return 0;
+}
