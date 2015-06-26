284c6d67d33cb1df4aaee62abdd5905072f38dfa
bothbossandboss <bothboss21@gmail.com>
2015-06-26 18:39:02 +0900

    insertを実装

--- fundamental_structure.cpp
+++ fundamental_structure.cpp
@@ -25,9 +25,19 @@ StudentData refer_data(int i){
 	return studentArray[i];
 }
 
+void insert_data(int position, StudentData data){
+	assert(nowStudentNum < MAX_STUDENT_COUNT);
+	//1つずつ後ろへずらす
+	for(int i=nowStudentNum;i>position;i--){
+		studentArray[i] = studentArray[i-1];
+	}
+	studentArray[position] = data;
+	nowStudentNum++;
+}
+
 int main(int argc, char *argv[]){
 	srand((unsigned)time(NULL));
-	nowStudentNum = rand() % MAX_STUDENT_COUNT;
+	nowStudentNum = rand() % MAX_STUDENT_COUNT + 1;
 	printf("first : %d\n", nowStudentNum);
 	for(int i=0;i<nowStudentNum;i++){
 		char c = 65 + i;
@@ -36,6 +46,19 @@ int main(int argc, char *argv[]){
 		studentArray[i].score = rand() % 100;
 	}
 	StudentData s = refer_data(rand() % nowStudentNum);
-	printf("%d, %s => %d\n", s.id, s.name, s.score);
+	printf("%d, %s => %d\n\nbefore insert\n", s.id, s.name, s.score);
+	int position = rand() % nowStudentNum;
+	for(int i= position-1;i<position+2;i++){
+		printf("%d, %c => %d\n", studentArray[i].id, studentArray[i].name, studentArray[i].score);
+	}
+	StudentData sample;
+	sample.id = 1000;
+	sample.name = "yasuhito";
+	sample.score = 100;
+	printf("\nafter insert\n");
+	insert_data(position, sample);
+	for(int i= position-1;i<position+2;i++){
+		printf("%d, %c => %d\n", studentArray[i].id, studentArray[i].name, studentArray[i].score);
+	}
 	return 0;
 }
