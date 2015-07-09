83b5e9f05c3874fdde0f3b69fddcfe90eefd4c25
bothbossandboss <bothboss21@gmail.com>
2015-07-09 15:07:15 +0900

    選択法を実装

--- sort_algorithm.cpp
+++ sort_algorithm.cpp
@@ -9,23 +9,48 @@
 
 using namespace std;
 
-void Swap(int data[], int a, int b){
+void makeData(int data[]){
+	for(int i=0;i<MAX_STUDENT_COUNT;i++){
+		data[i] = rand() % MAX_STUDENT_COUNT;
+	}
+}
+
+void printData(int data[]){
+	for(int i=0;i<MAX_STUDENT_COUNT;i++){
+		printf("%d ", data[i]);
+	}
+	printf("\n");
+}
+
+void mySwap(int data[], int a, int b){
 	int tmp = data[a];
 	data[a] = data[b];
 	data[b] = tmp;
 }
 
-void BubbleSort(int Data[], int n){
-	int BeginPlace, ComparePlace;
+void bubbleSort(int Data[], int n){
+	int beginPlace, comparePlace;
 	// 比較を始める位置を最初から n-1 まで変えていく
-	for(BeginPlace = 0;BeginPlace < n;BeginPlace++){
-		//n - BeginPlace まで比較する
-		for(ComparePlace = 1;ComparePlace < n - BeginPlace;ComparePlace++){
+	for(beginPlace = 0;beginPlace < n;beginPlace++){
+		//n - beginPlace まで比較する
+		for(comparePlace = 1;comparePlace < n - beginPlace;comparePlace++){
 			// 右のほうが小さかったら交換する
-			if(Data[ComparePlace - 1] > Data[ComparePlace]){
-				Swap(Data, ComparePlace-1, ComparePlace);
+			if(Data[comparePlace - 1] > Data[comparePlace]){
+				mySwap(Data, comparePlace-1, comparePlace);
 			}
 		}
+	}
+	return;
+}
+
+void selectionSort(int Data[], int n){
+	int min;
+	for(int i=0;i<MAX_STUDENT_COUNT;i++){
+		min = i;
+		for(int j=i+1;j<MAX_STUDENT_COUNT;j++){
+			if(Data[min] > Data[j]) min = j;
+		}
+		mySwap(Data, min, i);
 	}
 	return;
 }
@@ -33,13 +58,14 @@ void BubbleSort(int Data[], int n){
 int main(int argc, char *argv[]){
 	srand((unsigned)time(NULL));
 	int data[MAX_STUDENT_COUNT];
+	int another[MAX_STUDENT_COUNT];
+	makeData(data);
 	for(int i=0;i<MAX_STUDENT_COUNT;i++){
-		data[i] = rand() % MAX_STUDENT_COUNT;
+		another[i] = data[i];
 	}
-	BubbleSort(data, MAX_STUDENT_COUNT);
-	for(int i=0;i<MAX_STUDENT_COUNT;i++){
-		printf("%d ", data[i]);
-	}
-	printf("\n");
+	bubbleSort(data, MAX_STUDENT_COUNT);
+	printData(data);
+	selectionSort(another, MAX_STUDENT_COUNT);
+	printData(another);
 	return 0;
 }
