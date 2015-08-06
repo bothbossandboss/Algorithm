a7afd6ce93ca56abe2f442d29d53d5152e001639
bothbossandboss <bothboss21@gmail.com>
2015-08-06 17:28:07 +0900

    文字列比較用プログラムを作成

---
+++ compare_strings.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <assert.h>
+#include <iostream>
+
+#define MAX_STUDENT_COUNT 1000
+#define MAX_TURN 1000
+
+using namespace std;
+int main(int argc, char *argv[]){
+	srand((unsigned)time(NULL));
+}
